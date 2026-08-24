b8485f14270cef8bd62c84123861c77c1655b179
Fatjon Cecia <toncec24@gmail.com>
2026-08-24 11:40:44 +0200

    Allow Vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://vercel.com/fatjoncecias-projects/e-taxi-iq-albania/4V9A8mbNzpteBkbMfxU493jWfkzR"
     ],
     allow_credentials=True,
     allow_methods=["*"],
